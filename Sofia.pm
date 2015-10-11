0445f070403856575e55dca59a86ac4110e0ac4f
Michal Špaček <tupinek@gmail.com>
2015-10-11 10:38:21 +0200

    Add EXAMPLE4 to doc.

--- Sofia.pm
+++ Sofia.pm
@@ -195,6 +195,29 @@ For more information about Sofia Map, click L<here|https://en.wikipedia.org/wiki
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Sofia;
+
+ # Object.
+ my $obj = Map::Tube::Sofia->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Втори метродиаметър
+ # Първи метродиаметър
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
